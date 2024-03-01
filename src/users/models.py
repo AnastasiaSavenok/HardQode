f95d15872232b42e4684aa94a9b1b7999c8c4c97
AnastasiaSavenok <anastasiasavenok2828128@gmail.com>
2024-03-02 00:53:29 +0300

    Commit 4: Added CustomUser model

--- src/users/models.py
+++ src/users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from src.users import managers
+
+
+class CustomUser(AbstractUser):
+    class Roles(models.TextChoices):
+        STUDENT = 'student'
+        TEACHER = 'teacher'
+
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+    role = models.CharField(max_length=9, choices=Roles.choices, default=Roles.STUDENT)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = managers.CustomUserManager()
+
+    def __str__(self):
+        return self.email
